ff515bb020c958b36f707546d48f801f04d88a8c
nova_core_creatives <robertstrey@hotmail.com>
2022-03-22 22:33:19 -0500

    wording

--- public/js/getURL.js
+++ public/js/getURL.js
@@ -1,5 +1,5 @@
 
-function getURL(){
+window.onload = (event) =>{
   var currentLink = window.location;
   var textArea = document.getElementById('form-ref-link');
   textArea.innerHTML=currentLink;
